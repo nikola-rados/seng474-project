80e2cfb1f44b150442606a130b3e3447e62e7d7f
Nikola Rados <nikola.rados@s-sm.org>
2019-11-24 00:56:55 -0800

    Add package resources

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name = 'SENG474 Final Project',
     version = '0.1.0',
-    packages=['modules']
+    packages = find_packages(),
+    package_data = {'data': ['heroes.json', 'training_data.json', 'testing_data.json']}
 )
