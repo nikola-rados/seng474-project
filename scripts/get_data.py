3a37f7b46a013ab3e9d5304d56a9cc27922bb75e
Nikola Rados <nikola.rados@s-sm.org>
2019-11-23 23:52:40 -0800

    Add script for data collection

---
+++ scripts/get_data.py
@@ -0,0 +1,20 @@
+from modules import collect
+import argparse
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--key', '-k', help='API key', required=True)
+    parser.add_argument('--match-id', '-m', type=int,
+                        help='Starting match ID for lookup cycle',
+                        required=True)
+    parser.add_argument('--num-matches', '-n', default=5000, type=int,
+                        help='Number of matches to collect')
+    parser.add_argument('--outfile', '-o', default='match_data.json',
+                        help='Data output file')
+    args = parser.parse_args()
+
+    # Default settings when run were:
+    #   match_id = 5000012926
+    #   loops = 5000
+    collect_data(args.key, args.outfile, args.match_id, args.num_matches)
